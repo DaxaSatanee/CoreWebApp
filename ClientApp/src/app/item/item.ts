8c9f76911907a888adbaaff2d8b1d77688c44ac5
daxa <daxa.satanee@viprak.com>
2019-11-23 18:37:52 +0530

    # add new pages for item and code

---
+++ ClientApp/src/app/item/item.ts
@@ -0,0 +1,61 @@
+import { Component, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormControl } from '@angular/forms';
+
+@Component({
+	selector: 'app-item',
+	templateUrl: './item.html'
+})
+
+export class itemC {
+	public items: itemI[];
+	public myAppUrl: string = "";
+	public selectedItem: itemI = { id: 0, name: "", price: 0, oper: "add" };
+	//IName = new FormControl();
+
+
+	constructor(public http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+		this.myAppUrl = baseUrl;
+		http.get<itemI[]>(baseUrl + 'item/get').subscribe(result => {
+			this.items = result;
+		}, error => console.error(error));
+	};
+
+	saveItem(Id, Name, Price) {
+		var IId = parseInt(Id);
+		IId = isNaN(IId) ? 0 : IId;
+
+		const formData = new FormData();
+		formData.append('Id', IId.toString());
+		formData.append('Name', Name);
+
+		this.http
+			.post<itemI[]>(this.myAppUrl + 'item/save', formData)
+			.subscribe(data => {
+				this.items = data;
+			}, error => {
+				console.log(error);
+			});
+
+	}
+
+	editItem(item) {
+		this.selectedItem = item;
+	}
+	//errorHandler(error: Response) {
+	//	console.log(error);
+	//	return Observable.throw(error);
+	//}
+
+	//addItem(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+	//	http.post(baseUrl + 'item/save', { Id: "4", Name: "loo" }).subscribe(res => console.log(res));
+	//};
+
+}
+
+interface itemI {
+	id: number;
+	name: string;
+	price: number;
+	oper: string;
+}
---
+++ Controllers/ItemController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreWebApp.Models;
+using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace CoreWebApp.Controllers
+{
+    public class ItemController : Controller
+    {
+        private string v1;
+        //string v2 = _iConfig.GetValue<string>("DBConnection:ConnectionString");
+
+        public ItemController(IConfiguration iConfig)
+        {
+            v1 = iConfig.GetSection("DBConnection").GetSection("ConnectionString").Value;
+        }
+
+        [HttpGet]
+        public List<Item> Get()
+        {
+            var db = new SqlConnection(v1);
+
+            var sql = "select * from Item";
+            List<Item> items = db.Query<Item>(sql).ToList();
+
+            db.Close();
+            return items;
+        }
+
+        [HttpPost]
+        public List<Item> Save(Item item)
+        {
+            var db = new SqlConnection(v1);
+
+            ////var sql = "select * from Item";
+            ////List<Item> items = db.Query<Item>("Crud_Item", CommandType.StoredProcedure).ToList();
+
+            ////db.Close();
+            ////return items;
+
+            db.Open();
+            DynamicParameters param = new DynamicParameters();
+            param.Add("@Id", item.Id);
+            param.Add("@Name", item.Name);
+            param.Add("@Price", item.Price);
+            param.Add("@Oper", item.Oper);
+            List<Item> items = db.Query<Item>("Crud_Item", param, null, false, null, CommandType.StoredProcedure).ToList();
+            db.Close();
+            return items;
+        }
+    }
+}
